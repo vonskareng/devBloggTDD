c3386b46a5929d0cb0173999171a5f43af0a0041
Rebecka Skareng <rebecka.skareng@gmail.com>
2023-05-20 18:55:25 +0200

    added code so is_empty returns false for one added item

--- src/circular_buffer.py
+++ src/circular_buffer.py
@@ -2,3 +2,13 @@
 class CircularBuffer():
     def __init__(self, buffer_size):
         self.buffer_size = buffer_size
+        self.circular_buffer = []
+
+    def is_empty(self):
+        return len(self.circular_buffer) == 0
+    
+    
+    def enqueue(self, item):
+        self.circular_buffer = ["first Item"]
+
+
--- test/test_circular_buffer.py
+++ test/test_circular_buffer.py
@@ -1,9 +1,18 @@
 import pytest
 from src.circular_buffer import CircularBuffer
 
-def test_is_empty():
+def test_is_empty_returns_true():
     buffer_size = 5
-    circularBuffer = CircularBuffer(buffer_size)
+    circular_buffer = CircularBuffer(buffer_size)
     expected = True
-    actual = circularBuffer.is_empty()
+    actual = circular_buffer.is_empty()
+    assert actual == expected
+
+def test_is_empty_returns_false_for_one_added_item():
+    buffer_size = 5
+    item = "first Item"
+    circular_buffer = CircularBuffer(buffer_size)
+    circular_buffer.enqueue(item)
+    expected = False
+    actual = circular_buffer.is_empty()
     assert actual == expected
